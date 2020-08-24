95800bd68ab34b90cdc34b00c2b091f2a138af94
Irvin Lin <irvinln@gmail.com>
2020-08-24 10:50:35 +0800

    routing

--- src/App.js
+++ src/App.js
@@ -8,7 +8,15 @@ import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
-import Movies from './components/Movies'
+import SelectionType from './components/SelectionType'
+import Selection from './components/Selection'
+import Series from './components/Series'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -20,33 +28,59 @@ const useStyles = makeStyles((theme) => ({
   title: {
     flexGrow: 1,
   },
+  link: {
+    color: '#FFF',
+    textDecoration: 'none'
+  }
 }));
 
 
 const App = () => {
   const classes = useStyles();
-  return (
-    <div className={classes.root}>
-      <AppBar position="static">
-        <Toolbar>
-          {/* <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-            <MenuIcon />
-          </IconButton> */}
-          <Typography variant="h6" className={classes.title}>
-            DEMO Streaming
-          </Typography>
-          <Button color="inherit" style={{ marginRight: 8 }}>Login</Button>
-          <Button style={{ backgroundColor: 'black', color: 'white'}} variant="contained">Start Your Free Trial</Button>
-        </Toolbar>
-        <Toolbar style={{ backgroundColor: '#5b5b5b'}}>
+  return (    
+    <Router>
+      <div className={classes.root}>
+        <AppBar position="static">
+          <Toolbar>
+            {/* <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+              <MenuIcon />
+            </IconButton> */}
+            <Typography variant="h6" className={classes.title}>
+              DEMO Streaming
+            </Typography>
+            <Link to="/" className={classes.link} activeStyle={{color: 'red'}}>
+              <Button color="inherit" style={{ marginRight: 8 }}>Home</Button>
+            </Link>
+            <Link to="/series" className={classes.link}  activeStyle={{color: 'red'}}>
+              <Button color="inherit" style={{ marginRight: 8 }}>Series</Button>
+            </Link>
+            <Link to="/movies" className={classes.link}  activeStyle={{color: 'red'}}>
+              <Button color="inherit" style={{ marginRight: 8 }}>Movies</Button>
+            </Link>
+          </Toolbar>
+        </AppBar>
+        {/* <Selection /> */}
+        {/* <Movies /> */}
+        <Toolbar style={{ backgroundColor: '#5b5b5b', color: 'white'}}>
           <Typography variant="body1" className={classes.title}>
-            Popular Titles
+            Popular Titles 
           </Typography>
         </Toolbar>
-      </AppBar>
-      <Movies />
-    </div>
+      </div>
+      <Switch>
+          <Route path="/series">
+            <SelectionType type="series" />
+          </Route>
+          <Route path="/movies">
+            <SelectionType type="movie" />
+          </Route>
+          <Route path="/">
+            <Selection />
+          </Route>
+        </Switch>
+    </Router>
   );
 }
+
 
 export default App;
